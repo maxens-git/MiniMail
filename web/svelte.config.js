2770164504bcba7f7086f905c5ca563cbdb6d1cf
Maxens Verron <maxens@MacBook-Air-de-Maxens.local>
2025-07-12 20:47:39 +0200

    build

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	preprocess: preprocess(),
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
